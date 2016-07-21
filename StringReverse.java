a5653a9f1b4b6011c9f6951914e9aedde294a1f6
MathanKumarS <mathankumar.scse@ksrce.ac.in>
2016-07-21 09:36:46 +0530

    Create StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,18 @@
+# SMK
+
+import java.util.Scanner;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		  Scanner s = new Scanner(System.in);
+		  System.out.println("Enter the string to reverse");
+		  String input = s.next();
+		  StringBuilder input1 = new StringBuilder();
+		  input1.append(input);
+		  input1=input1.reverse(); 
+		  for (int i=0;i<input1.length();i++)
+		  System.out.print(input1.charAt(i)); 
+	}
+
+}
