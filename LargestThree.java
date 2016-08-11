7f79c6dc0f826d923e710835aa8e380ccb817709
MathanKumarS <mathankumar.scse@ksrce.ac.in>
2016-08-11 21:08:26 +0530

    Create LargestThree.java

---
+++ LargestThree.java
@@ -0,0 +1,24 @@
+# SMK
+
+import java.util.Scanner;
+
+public class LargestThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = s.nextInt();
+		if(a>b && a>c){
+			System.out.println(a);
+		}
+		else if(b>c){
+			System.out.println(b);
+		}
+		else{
+			System.out.println(c);
+		}
+	}
+
+}
