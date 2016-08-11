f50e66257d3222a3da5b0d4831fa30c5499f5d73
MathanKumarS <mathankumar.scse@ksrce.ac.in>
2016-08-11 21:22:20 +0530

    Create PowerOfNumber.java

---
+++ PowerOfNumber.java
@@ -0,0 +1,14 @@
+# SMK
+
+import java.util.Scanner;
+
+public class PowerOfNumber {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		double d1 = s.nextDouble();
+		double d2 = s.nextDouble();
+		System.out.println(Math.pow(d1, d2));
+	}
+
+}
